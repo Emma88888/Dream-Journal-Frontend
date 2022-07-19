8e5f8cdbeb39f95d75ec47c5b8f484182672d929
Emma <langbert95@gmail.com>
2022-07-19 11:52:25 -0400

    undo previous

--- src/NewDreams/NewDreamText.js
+++ src/NewDreams/NewDreamText.js
@@ -2,7 +2,7 @@ import React from "react"
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const api = "dream-journal.netlify.com/api/dream"
+const api = "http://localhost:4040/api/dream"
 
 const NewDreamText = () => {
     const navigate = useNavigate()
