1aca9c104b0cf5d47dcc8aa8f1250dff17acdede
Emma <langbert95@gmail.com>
2022-07-20 09:20:44 -0400

    styling

--- src/Dream/Dream.js
+++ src/Dream/Dream.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
+import "./style.css"
 
 const api = "https://dream-journal-8.herokuapp.com/api/dream"
 
